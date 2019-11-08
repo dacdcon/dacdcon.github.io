ff246428fe59f4ebbe71991c861ecf7909cf3cb6
dacdcon <57398882+dacdcon@users.noreply.github.com>
2019-11-08 10:40:08 +0530

    Add files via upload

---
+++ Experiment/init.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA8W4qR3V2lS0Zvlf3jN33IULvqsQit2MY",
+    authDomain: "dacdcon.firebaseapp.com",
+    databaseURL: "https://dacdcon.firebaseio.com",
+    projectId: "dacdcon",
+    storageBucket: "dacdcon.appspot.com",
+    messagingSenderId: "527270794137",
+    appId: "1:527270794137:web:4446bb5e24346985717908",
+    measurementId: "G-HNMH4RT819"
+    };
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
